1752cf14cfa70c291c99ccf5f6a9ce86152cc3b0
Raul Myron <39887348+RaulMyron@users.noreply.github.com>
2020-10-15 22:06:29 -0300

    Create Lavar-maos.ino

---
+++ Lava/Lavar-maos.ino
@@ -0,0 +1,57 @@
+#include <LiquidCrystal.h>
+
+int cm = 0;
+int motor = 13;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  return pulseIn(echoPin, HIGH);
+  
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(motor, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Lave suas maos");
+  lcd.setCursor(0,1);
+  lcd.print("Antes de entrar");
+}
+
+void loop()
+{
+  
+  cm = 0.01723 * readUltrasonicDistance(8, 8);
+  delay(1000);
+  Serial.print(cm);
+  Serial.println("cm");
+
+  if(cm<21){
+ 	lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Lavando...");
+    digitalWrite(motor, HIGH);
+    lcd.setCursor(0,1);
+    lcd.print("Use mascara! :)");
+  	}
+  else{
+    digitalWrite(motor, LOW);
+    lcd.clear();
+  	lcd.setCursor(0, 0);
+  	lcd.print("Lave suas maos");
+ 	lcd.setCursor(0,1);
+  	lcd.print("Antes de entrar");
+    delay(500);
+  }
+}
